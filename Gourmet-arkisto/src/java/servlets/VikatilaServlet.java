f4f873925fa2b9f209177d5f968197150ad46671
Hopeata <valeria.verkhovskaia@gmail.com>
2013-11-22 00:28:24 +0200

    Virhetilanteiden käsittely

---
+++ Gourmet-arkisto/src/java/servlets/VikatilaServlet.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Valeria
+ */
+public class VikatilaServlet extends YleisServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Throwable throwable = (Throwable) req.getAttribute("javax.servlet.error.exception");
+        lisaaVirheViesti(req, throwable.getMessage());
+        siirrySivulle("/kirjautuminen", req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
