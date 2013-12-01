23ce38da5f940ba11921a318c719bd9873a43842
Hopeata <valeria.verkhovskaia@gmail.com>
2013-12-02 01:16:40 +0200

    Lisätty merkistö-filtteri, jolla saa skandit nätisti kantaan

---
+++ Gourmet-arkisto/src/java/filters/MerkistoFilter.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author Valeria
+ */
+public class MerkistoFilter implements Filter {
+
+    private String encoding = "utf-8";
+
+    public void doFilter(ServletRequest request,
+            ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        request.setCharacterEncoding(encoding);
+        filterChain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String encodingParam = filterConfig.getInitParameter("encoding");
+        if (encodingParam != null) {
+            encoding = encodingParam;
+        }
+    }
+
+    public void destroy() {
+        // nothing todo
+    }
+}
